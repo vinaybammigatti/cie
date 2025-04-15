cd7f6ac683e14224399cd44b0f2c03a38c3112b6
vinaybammigatti <vinaybammigatti062@gmail.com>
2025-04-15 10:26:13 +0530

    saved

---
+++ ArithmeticOperation.java
@@ -0,0 +1,17 @@
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Arithmetic Operations:");
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Division: " + division);
+    }
+}
